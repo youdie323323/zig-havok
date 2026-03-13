929b13ec1297e3dd1c990af39842ea294c7c0760
youdi323323 <kouki32312171217@gmail.com>
2026-03-14 00:18:58 +0900

    mood

--- build.zig
+++ build.zig
@@ -11,6 +11,7 @@ pub fn build(b: *std.Build) void {
     const mod = b.addModule("havok", .{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
+        .optimize = optimize,
     });
 
     const exe = b.addExecutable(.{
